c7e95eb908c9594759c6c299854c6f900f5a832f
frank-LIU8213 <liufrank1226@gmail.com>
2026-05-14 22:10:56 +0800

    ensemble w1: NEX-24

--- js/audio.js
+++ js/audio.js
@@ -4,11 +4,29 @@
 
   /** @type {import('./contract').AudioAPI} */
   Pomodoro.Audio = {
+    _sounds: {},
+
     init() {
-      throw new Error('Not implemented: Audio.init');
+      // Preload audio files
+      this._sounds['timerEnd'] = new Audio('sounds/end.mp3');
+      this._sounds['click']    = new Audio('sounds/click.mp3');
+
+      // Mark preload – most browsers support it by default
+      Object.values(this._sounds).forEach(audio => audio.preload = 'auto');
     },
+
     playSound(name) {
-      throw new Error('Not implemented: Audio.playSound');
+      const audio = this._sounds[name];
+      if (!audio) {
+        console.warn(`[Pomodoro.Audio] Unknown sound: "${name}"`);
+        return;
+      }
+      // Reset to beginning in case it's already playing
+      audio.currentTime = 0;
+      audio.play().catch(err => {
+        // Browsers may block autoplay – silent failure is acceptable here
+        console.warn(`[Pomodoro.Audio] Could not play "${name}":`, err.message);
+      });
     }
   };
 })();
