e243421f9da5029d9a27870a1d3c2f86a8d14c17
frank-LIU8213 <liufrank1226@gmail.com>
2026-05-16 09:03:34 +0800

    ensemble w1: NEX-28

--- snake/beans.py
+++ snake/beans.py
@@ -5,4 +5,54 @@ from .models import BeanType
 
 def init_beans() -> List[BeanType]:
     """Return all available bean types with probabilities."""
-    return []
+    return [
+        BeanType(
+            name="normal",
+            symbol="O",
+            color=1,
+            effect=None,
+            probability=0.50,
+        ),
+        BeanType(
+            name="speed_up",
+            symbol="S",
+            color=2,
+            effect="speed_up",
+            probability=0.10,
+        ),
+        BeanType(
+            name="slow_down",
+            symbol="W",
+            color=3,
+            effect="slow_down",
+            probability=0.10,
+        ),
+        BeanType(
+            name="invert",
+            symbol="I",
+            color=4,
+            effect="invert",
+            probability=0.08,
+        ),
+        BeanType(
+            name="shield",
+            symbol="D",
+            color=5,
+            effect="shield",
+            probability=0.08,
+        ),
+        BeanType(
+            name="shrink",
+            symbol="R",
+            color=6,
+            effect="shrink",
+            probability=0.07,
+        ),
+        BeanType(
+            name="double_score",
+            symbol="M",
+            color=7,
+            effect="double_score",
+            probability=0.07,
+        ),
+    ]
