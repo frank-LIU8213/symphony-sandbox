1d35cec780480c7e582c0a534b46eadca98fbd44
frank-LIU8213 <liufrank1226@gmail.com>
2026-05-18 21:51:10 +0800

    ensemble w0: NEX-32

--- engine.js
+++ engine.js
@@ -5,6 +5,10 @@ class AudioController {
     constructor() {
         this.sounds = new Map();
         this.globalVolume = 1.0;
+        this.ctx = new (window.AudioContext || window.webkitAudioContext)();
+        this.masterGain = this.ctx.createGain();
+        this.masterGain.connect(this.ctx.destination);
+        this.masterGain.gain.value = this.globalVolume;
     }
 
     /**
@@ -48,9 +52,47 @@ class AudioController {
      */
     setGlobalVolume(vol) {
         this.globalVolume = vol;
+        this.masterGain.gain.value = vol;
         this.sounds.forEach(audio => {
-            audio.volume = vol * (audio._baseVolume || 1);
+            audio.volume = vol;
         });
+    }
+
+    /**
+     * Play a synthesized ambient sound for the tech theme.
+     * @param {string} type - 'drone' or 'shimmer'
+     * @returns {Object} - Reference to stop it later
+     */
+    playAmbient(type) {
+        if (this.ctx.state === 'suspended') this.ctx.resume();
+        const osc = this.ctx.createOscillator();
+        const gain = this.ctx.createGain();
+        const filter = this.ctx.createBiquadFilter();
+        
+        filter.type = 'lowpass';
+        filter.frequency.value = type === 'drone' ? 150 : 600;
+        
+        osc.type = type === 'drone' ? 'sine' : 'triangle';
+        osc.frequency.value = type === 'drone' ? 55 : 330;
+        
+        gain.gain.value = 0.08;
+        
+        osc.connect(filter);
+        filter.connect(gain);
+        gain.connect(this.masterGain);
+        
+        osc.start();
+        
+        const lfo = this.ctx.createOscillator();
+        lfo.type = 'sine';
+        lfo.frequency.value = type === 'drone' ? 0.05 : 0.3;
+        const lfoGain = this.ctx.createGain();
+        lfoGain.gain.value = type === 'drone' ? 8 : 40;
+        lfo.connect(lfoGain);
+        lfoGain.connect(osc.frequency);
+        lfo.start();
+        
+        return { stop: () => { osc.stop(); lfo.stop(); } };
     }
 }
 
